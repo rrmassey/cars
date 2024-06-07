fbb934b2b8903642313551824fe9578031fe77c6
rrmassey <157766293+rrmassey@users.noreply.github.com>
2024-06-07 16:53:15 -0700

    Update PerformanceCar.cpp

--- PerformanceCar.cpp
+++ PerformanceCar.cpp
@@ -1,77 +1,65 @@
-#ifndef PERFORMANCECAR_H
-#define PERFORMANCECAR_H
+#include "PerformanceCar.h"
+#include "InvalidValueException.h"
 
-#include "Car.h"
-#include "Engine.h"
+Engine PerformanceCar::getEngine() const {
+    return engine;
+}
 
-class PerformanceCar : public Car<int> {
-private:
-    Engine engine;
-    double topSpeed;
-    double zeroToSixty;
-    double quarterMile;
+double PerformanceCar::getTopSpeed() const {
+    return topSpeed;
+}
 
-public:
-    PerformanceCar(int id = 0, const std::string& make = "", const std::string& model = "", 
-                   const Engine& engine = Engine(), double topSpeed = 0.0, double zeroToSixty = 0.0, double quarterMile = 0.0)
-        : Car(id, make, model), engine(engine), topSpeed(topSpeed), zeroToSixty(zeroToSixty), quarterMile(quarterMile) {}
+double PerformanceCar::getZeroToSixty() const {
+    return zeroToSixty;
+}
 
-    Engine getEngine() const { return engine; }
-    double getTopSpeed() const { return topSpeed; }
-    double getZeroToSixty() const { return zeroToSixty; }
-    double getQuarterMile() const { return quarterMile; }
+double PerformanceCar::getQuarterMile() const {
+    return quarterMile;
+}
 
-    void setEngine(const Engine& engine) { this->engine = engine; }
-    void setTopSpeed(double topSpeed) { this->topSpeed = topSpeed; }
-    void setZeroToSixty(double zeroToSixty) { this->zeroToSixty = zeroToSixty; }
-    void setQuarterMile(double quarterMile) { this->quarterMile = quarterMile; }
+void PerformanceCar::setEngine(const Engine& engine) {
+    this->engine = engine;
+}
 
-    void display() const override {
-        Car::display();
-        engine.display();
-        std::cout << "Top Speed: " << topSpeed << " mph"
-                  << "\n0-60 mph: " << zeroToSixty << " seconds"
-                  << "\nQuarter Mile: " << quarterMile << " seconds" << std::endl;
+void PerformanceCar::setTopSpeed(double topSpeed) {
+    this->topSpeed = topSpeed;
+}
+
+void PerformanceCar::setZeroToSixty(double zeroToSixty) {
+    this->zeroToSixty = zeroToSixty;
+}
+
+void PerformanceCar::setQuarterMile(double quarterMile) {
+    this->quarterMile = quarterMile;
+}
+
+void PerformanceCar::display() const {
+    Car::display();
+    engine.display();
+    std::cout << "Top Speed: " << topSpeed << " mph"
+              << "\n0-60 mph: " << zeroToSixty << " seconds"
+              << "\nQuarter Mile: " << quarterMile << " seconds" << std::endl;
+}
+
+void PerformanceCar::readFromBinary(std::ifstream& in) {
+    Car::readFromBinary(in);
+    engine.readFromBinary(in);
+    in.read(reinterpret_cast<char*>(&topSpeed), sizeof(topSpeed));
+    in.read(reinterpret_cast<char*>(&zeroToSixty), sizeof(zeroToSixty));
+    in.read(reinterpret_cast<char*>(&quarterMile), sizeof(quarterMile));
+}
+
+void PerformanceCar::writeToBinary(std::ofstream& out) const {
+    Car::writeToBinary(out);
+    engine.writeToBinary(out);
+    out.write(reinterpret_cast<const char*>(&topSpeed), sizeof(topSpeed));
+    out.write(reinterpret_cast<const char*>(&zeroToSixty), sizeof(zeroToSixty));
+    out.write(reinterpret_cast<const char*>(&quarterMile), sizeof(quarterMile));
+}
+
+void PerformanceCar::validate() const {
+    if (topSpeed < 0 || zeroToSixty < 0 || quarterMile < 0) {
+        throw InvalidValueException("Invalid value in PerformanceCar");
     }
-
-    void readFromBinary(std::ifstream& in) override {
-        Car::readFromBinary(in);
-        engine.readFromBinary(in);
-        in.read(reinterpret_cast<char*>(&topSpeed), sizeof(topSpeed));
-        in.read(reinterpret_cast<char*>(&zeroToSixty), sizeof(zeroToSixty));
-        in.read(reinterpret_cast<char*>(&quarterMile), sizeof(quarterMile));
-    }
-
-    void writeToBinary(std::ofstream& out) const override {
-        Car::writeToBinary(out);
-        engine.writeToBinary(out);
-        out.write(reinterpret_cast<const char*>(&topSpeed), sizeof(topSpeed));
-        out.write(reinterpret_cast<const char*>(&zeroToSixty), sizeof(zeroToSixty));
-        out.write(reinterpret_cast<const char*>(&quarterMile), sizeof(quarterMile));
-    }
-
-    void validate() const override {
-        if (topSpeed < 0 || zeroToSixty < 0 || quarterMile < 0) {
-            throw InvalidValueException("Invalid value in PerformanceCar");
-        }
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const PerformanceCar& car) {
-        car.display();
-        return os;
-    }
-
-    friend std::istream& operator>>(std::istream& is, PerformanceCar& car) {
-        car.read(is);
-        car.engine.read(is);
-        std::cout << "Enter Top Speed (mph): ";
-        is >> car.topSpeed;
-        std::cout << "Enter 0-60 mph time (seconds): ";
-        is >> car.zeroToSixty;
-        std::cout << "Enter Quarter Mile time (seconds): ";
-        is >> car.quarterMile;
-        return is;
-    }
-};
-
-#endif // PERFORMANCECAR_H
+    engine.validate();
+}
