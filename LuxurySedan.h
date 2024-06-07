a6622c742cd68a899a3f7e0c7ca4cd4e13d95d23
rrmassey <157766293+rrmassey@users.noreply.github.com>
2024-06-07 16:51:25 -0700

    Update LuxurySedan.h

--- LuxurySedan.h
+++ LuxurySedan.h
@@ -5,28 +5,74 @@
 
 class LuxurySedan : public Car<int> {
 private:
-    int year;
-    double price;
-    std::string features;
+    std::string interiorMaterial;
+    bool hasMassageSeats;
+    std::string infotainmentSystem;
+    std::string paintColor;
+
 public:
-    LuxurySedan(int id = 0, const std::string& make = "", const std::string& model = "", int year = 0, double price = 0.0, const std::string& features = "");
+    LuxurySedan(int id = 0, const std::string& make = "", const std::string& model = "", 
+                const std::string& interiorMaterial = "", bool hasMassageSeats = false, 
+                const std::string& infotainmentSystem = "", const std::string& paintColor = "")
+        : Car(id, make, model), interiorMaterial(interiorMaterial), hasMassageSeats(hasMassageSeats), 
+          infotainmentSystem(infotainmentSystem), paintColor(paintColor) {}
 
-    void display() const override;
-    void readFromBinary(std::ifstream& in) override;
-    void writeToBinary(std::ofstream& out) const override;
+    std::string getInteriorMaterial() const { return interiorMaterial; }
+    bool getHasMassageSeats() const { return hasMassageSeats; }
+    std::string getInfotainmentSystem() const { return infotainmentSystem; }
+    std::string getPaintColor() const { return paintColor; }
 
-    int getYear() const;
-    double getPrice() const;
-    std::string getFeatures() const;
+    void setInteriorMaterial(const std::string& interiorMaterial) { this->interiorMaterial = interiorMaterial; }
+    void setHasMassageSeats(bool hasMassageSeats) { this->hasMassageSeats = hasMassageSeats; }
+    void setInfotainmentSystem(const std::string& infotainmentSystem) { this->infotainmentSystem = infotainmentSystem; }
+    void setPaintColor(const std::string& paintColor) { this->paintColor = paintColor; }
 
-    void setYear(int year);
-    void setPrice(double price);
-    void setFeatures(const std::string& features);
+    void display() const override {
+        Car::display();
+        std::cout << "Interior Material: " << interiorMaterial 
+                  << "\nHas Massage Seats: " << (hasMassageSeats ? "Yes" : "No")
+                  << "\nInfotainment System: " << infotainmentSystem 
+                  << "\nPaint Color: " << paintColor << std::endl;
+    }
 
-    void validate() const override;
+    void readFromBinary(std::ifstream& in) override {
+        Car::readFromBinary(in);
+        std::getline(in, interiorMaterial, '\0');
+        in.read(reinterpret_cast<char*>(&hasMassageSeats), sizeof(hasMassageSeats));
+        std::getline(in, infotainmentSystem, '\0');
+        std::getline(in, paintColor, '\0');
+    }
 
-    friend std::ostream& operator<<(std::ostream& os, const LuxurySedan& car);
-    friend std::istream& operator>>(std::istream& is, LuxurySedan& car);
+    void writeToBinary(std::ofstream& out) const override {
+        Car::writeToBinary(out);
+        out.write(interiorMaterial.c_str(), interiorMaterial.size() + 1);
+        out.write(reinterpret_cast<const char*>(&hasMassageSeats), sizeof(hasMassageSeats));
+        out.write(infotainmentSystem.c_str(), infotainmentSystem.size() + 1);
+        out.write(paintColor.c_str(), paintColor.size() + 1);
+    }
+
+    void validate() const override {
+        // You can add more validations as required
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const LuxurySedan& car) {
+        car.display();
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, LuxurySedan& car) {
+        car.read(is);
+        std::cout << "Enter Interior Material: ";
+        std::getline(is, car.interiorMaterial);
+        std::cout << "Has Massage Seats (1 for Yes, 0 for No): ";
+        is >> car.hasMassageSeats;
+        is.ignore();
+        std::cout << "Enter Infotainment System: ";
+        std::getline(is, car.infotainmentSystem);
+        std::cout << "Enter Paint Color: ";
+        std::getline(is, car.paintColor);
+        return is;
+    }
 };
 
 #endif // LUXURYSEDAN_H
