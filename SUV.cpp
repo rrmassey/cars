2161fa957a5386647b1b17619debf4cb5da122b3
rrmassey <157766293+rrmassey@users.noreply.github.com>
2024-06-07 16:53:00 -0700

    Update SUV.cpp

--- SUV.cpp
+++ SUV.cpp
@@ -1,84 +1,64 @@
 #include "SUV.h"
+#include "InvalidValueException.h"
 
-SUV::SUV(int id, const std::string& make, const std::string& model, int year, double price, int seatingCapacity, double fuelEfficiency)
-    : Car(id, make, model), year(year), price(price), seatingCapacity(seatingCapacity), fuelEfficiency(fuelEfficiency) {}
+int SUV::getGroundClearance() const {
+    return groundClearance;
+}
+
+double SUV::getCargoCapacity() const {
+    return cargoCapacity;
+}
+
+bool SUV::getFourWheelDrive() const {
+    return fourWheelDrive;
+}
+
+double SUV::getTowingCapacity() const {
+    return towingCapacity;
+}
+
+void SUV::setGroundClearance(int groundClearance) {
+    this->groundClearance = groundClearance;
+}
+
+void SUV::setCargoCapacity(double cargoCapacity) {
+    this->cargoCapacity = cargoCapacity;
+}
+
+void SUV::setFourWheelDrive(bool fourWheelDrive) {
+    this->fourWheelDrive = fourWheelDrive;
+}
+
+void SUV::setTowingCapacity(double towingCapacity) {
+    this->towingCapacity = towingCapacity;
+}
 
 void SUV::display() const {
     Car::display();
-    std::cout << "Year: " << year << std::endl;
-    std::cout << "Price: $" << price << std::endl;
-    std::cout << "Seating Capacity: " << seatingCapacity << std::endl;
-    std::cout << "Fuel Efficiency: " << fuelEfficiency << " mpg" << std::endl;
+    std::cout << "Ground Clearance: " << groundClearance << " inches" 
+              << "\nCargo Capacity: " << cargoCapacity << " cu ft" 
+              << "\nFour-Wheel Drive: " << (fourWheelDrive ? "Yes" : "No")
+              << "\nTowing Capacity: " << towingCapacity << " lbs" << std::endl;
 }
 
 void SUV::readFromBinary(std::ifstream& in) {
     Car::readFromBinary(in);
-    in.read(reinterpret_cast<char*>(&year), sizeof(year));
-    in.read(reinterpret_cast<char*>(&price), sizeof(price));
-    in.read(reinterpret_cast<char*>(&seatingCapacity), sizeof(seatingCapacity));
-    in.read(reinterpret_cast<char*>(&fuelEfficiency), sizeof(fuelEfficiency));
+    in.read(reinterpret_cast<char*>(&groundClearance), sizeof(groundClearance));
+    in.read(reinterpret_cast<char*>(&cargoCapacity), sizeof(cargoCapacity));
+    in.read(reinterpret_cast<char*>(&fourWheelDrive), sizeof(fourWheelDrive));
+    in.read(reinterpret_cast<char*>(&towingCapacity), sizeof(towingCapacity));
 }
 
 void SUV::writeToBinary(std::ofstream& out) const {
     Car::writeToBinary(out);
-    out.write(reinterpret_cast<const char*>(&year), sizeof(year));
-    out.write(reinterpret_cast<const char*>(&price), sizeof(price));
-    out.write(reinterpret_cast<const char*>(&seatingCapacity), sizeof(seatingCapacity));
-    out.write(reinterpret_cast<const char*>(&fuelEfficiency), sizeof(fuelEfficiency));
-}
-
-int SUV::getYear() const {
-    return year;
-}
-
-double SUV::getPrice() const {
-    return price;
-}
-
-int SUV::getSeatingCapacity() const {
-    return seatingCapacity;
-}
-
-double SUV::getFuelEfficiency() const {
-    return fuelEfficiency;
-}
-
-void SUV::setYear(int year) {
-    this->year = year;
-}
-
-void SUV::setPrice(double price) {
-    this->price = price;
-}
-
-void SUV::setSeatingCapacity(int seatingCapacity) {
-    this->seatingCapacity = seatingCapacity;
-}
-
-void SUV::setFuelEfficiency(double fuelEfficiency) {
-    this->fuelEfficiency = fuelEfficiency;
+    out.write(reinterpret_cast<const char*>(&groundClearance), sizeof(groundClearance));
+    out.write(reinterpret_cast<const char*>(&cargoCapacity), sizeof(cargoCapacity));
+    out.write(reinterpret_cast<const char*>(&fourWheelDrive), sizeof(fourWheelDrive));
+    out.write(reinterpret_cast<const char*>(&towingCapacity), sizeof(towingCapacity));
 }
 
 void SUV::validate() const {
-    if (year < 1886 || price < 0 || seatingCapacity < 0 || fuelEfficiency < 0) {
+    if (groundClearance < 0 || cargoCapacity < 0 || towingCapacity < 0) {
         throw InvalidValueException("Invalid value in SUV");
     }
-}
-
-std::ostream& operator<<(std::ostream& os, const SUV& suv) {
-    suv.display();
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, SUV& suv) {
-    is >> static_cast<Car<int>&>(suv);
-    std::cout << "Enter Year: ";
-    is >> suv.year;
-    std::cout << "Enter Price: ";
-    is >> suv.price;
-    std::cout << "Enter Seating Capacity: ";
-    is >> suv.seatingCapacity;
-    std::cout << "Enter Fuel Efficiency (mpg): ";
-    is >> suv.fuelEfficiency;
-    return is;
 }
