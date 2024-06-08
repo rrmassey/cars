b527ae9cb3f82e588f614e99cce3bd8a2f1abab0
rrmassey <157766293+rrmassey@users.noreply.github.com>
2024-06-07 17:29:27 -0700

    Update Engine.h

--- Engine.h
+++ Engine.h
@@ -7,50 +7,29 @@
 class Engine {
 private:
     int horsepower;
-    double displacement;
+    double displacement;  //in liters
     int numCylinders;
     int torque;
 
 public:
-    Engine(int horsepower = 0, double displacement = 0.0, int numCylinders = 0, int torque = 0)
-        : horsepower(horsepower), displacement(displacement), numCylinders(numCylinders), torque(torque) {}
+    Engine(int horsepower = 0, double displacement = 0.0);
 
-    int getHorsepower() const { return horsepower; }
-    double getDisplacement() const { return displacement; }
-    int getNumCylinders() const { return numCylinders; }
-    int getTorque() const { return torque; }
+    void display() const;
+    void readFromBinary(std::ifstream& in);
+    void writeToBinary(std::ofstream& out) const;
 
-    void setHorsepower(int horsepower) { this->horsepower = horsepower; }
-    void setDisplacement(double displacement) { this->displacement = displacement; }
-    void setNumCylinders(int numCylinders) { this->numCylinders = numCylinders; }
-    void setTorque(int torque) { this->torque = torque; }
+    int getHorsepower() const;
+    double getDisplacement() const;
+    int getNumCylinders() const;
+    int getTorque() const;
 
-    void display() const {
-        std::cout << "Horsepower: " << horsepower 
-                  << "\nDisplacement: " << displacement << " L"
-                  << "\nNumber of Cylinders: " << numCylinders 
-                  << "\nTorque: " << torque << " lb-ft" << std::endl;
-    }
+    void setHorsepower(int horsepower);
+    void setDisplacement(double displacement);
+    void setNumCylinders(int numCylinders);
+    void setTorque(int torque);
 
-    void readFromBinary(std::ifstream& in) {
-        in.read(reinterpret_cast<char*>(&horsepower), sizeof(horsepower));
-        in.read(reinterpret_cast<char*>(&displacement), sizeof(displacement));
-        in.read(reinterpret_cast<char*>(&numCylinders), sizeof(numCylinders));
-        in.read(reinterpret_cast<char*>(&torque), sizeof(torque));
-    }
-
-    void writeToBinary(std::ofstream& out) const {
-        out.write(reinterpret_cast<const char*>(&horsepower), sizeof(horsepower));
-        out.write(reinterpret_cast<const char*>(&displacement), sizeof(displacement));
-        out.write(reinterpret_cast<const char*>(&numCylinders), sizeof(numCylinders));
-        out.write(reinterpret_cast<const char*>(&torque), sizeof(torque));
-    }
-
-    void validate() const {
-        if (horsepower < 0 || displacement < 0 || numCylinders < 0 || torque < 0) {
-            throw InvalidValueException("Invalid value in Engine");
-        }
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Engine& engine);
+    friend std::istream& operator>>(std::istream& is, Engine& engine);
 };
 
 #endif // ENGINE_H
