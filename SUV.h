d16986380877a38be8fea2e7faf313c7af316c3b
rrmassey <157766293+rrmassey@users.noreply.github.com>
2024-06-07 17:30:33 -0700

    Update SUV.h

--- SUV.h
+++ SUV.h
@@ -1,78 +1,36 @@
 #ifndef SUV_H
 #define SUV_H
 
-#include "Car.h"
+#include "car.h"
 
 class SUV : public Car<int> {
 private:
-    int groundClearance;
-    double cargoCapacity;
+    int groundClearance;  //in inches
+    double cargoCapacity;  //in cubic feet
     bool fourWheelDrive;
-    double towingCapacity;
+    double towingCapacity;  //in pounds
 
 public:
-    SUV(int id = 0, const std::string& make = "", const std::string& model = "", 
-        int groundClearance = 0, double cargoCapacity = 0.0, bool fourWheelDrive = false, double towingCapacity = 0.0)
-        : Car(id, make, model), groundClearance(groundClearance), cargoCapacity(cargoCapacity), 
-          fourWheelDrive(fourWheelDrive), towingCapacity(towingCapacity) {}
+    SUV(int id = 0, const std::string& make = "", const std::string& model = "", int groundClearance = 0, double cargoCapacity = 0.0, bool fourWheelDrive = false, double towingCapacity = 0.0);
 
-    int getGroundClearance() const { return groundClearance; }
-    double getCargoCapacity() const { return cargoCapacity; }
-    bool getFourWheelDrive() const { return fourWheelDrive; }
-    double getTowingCapacity() const { return towingCapacity; }
+    void display() const override;
+    void readFromBinary(std::ifstream& in) override;
+    void writeToBinary(std::ofstream& out) const override;
 
-    void setGroundClearance(int groundClearance) { this->groundClearance = groundClearance; }
-    void setCargoCapacity(double cargoCapacity) { this->cargoCapacity = cargoCapacity; }
-    void setFourWheelDrive(bool fourWheelDrive) { this->fourWheelDrive = fourWheelDrive; }
-    void setTowingCapacity(double towingCapacity) { this->towingCapacity = towingCapacity; }
+    int getGroundClearance() const;
+    double getCargoCapacity() const;
+    bool getFourWheelDrive() const;
+    double getTowingCapacity() const;
 
-    void display() const override {
-        Car::display();
-        std::cout << "Ground Clearance: " << groundClearance << " inches"
-                  << "\nCargo Capacity: " << cargoCapacity << " cu ft"
-                  << "\nFour-Wheel Drive: " << (fourWheelDrive ? "Yes" : "No")
-                  << "\nTowing Capacity: " << towingCapacity << " lbs" << std::endl;
-    }
+    void setGroundClearance(int groundClearance);
+    void setCargoCapacity(double cargoCapacity);
+    void setFourWheelDrive(bool fourWheelDrive);
+    void setTowingCapacity(double towingCapacity);
 
-    void readFromBinary(std::ifstream& in) override {
-        Car::readFromBinary(in);
-        in.read(reinterpret_cast<char*>(&groundClearance), sizeof(groundClearance));
-        in.read(reinterpret_cast<char*>(&cargoCapacity), sizeof(cargoCapacity));
-        in.read(reinterpret_cast<char*>(&fourWheelDrive), sizeof(fourWheelDrive));
-        in.read(reinterpret_cast<char*>(&towingCapacity), sizeof(towingCapacity));
-    }
+    void validate() const override;
 
-    void writeToBinary(std::ofstream& out) const override {
-        Car::writeToBinary(out);
-        out.write(reinterpret_cast<const char*>(&groundClearance), sizeof(groundClearance));
-        out.write(reinterpret_cast<const char*>(&cargoCapacity), sizeof(cargoCapacity));
-        out.write(reinterpret_cast<const char*>(&fourWheelDrive), sizeof(fourWheelDrive));
-        out.write(reinterpret_cast<const char*>(&towingCapacity), sizeof(towingCapacity));
-    }
-
-    void validate() const override {
-        if (groundClearance < 0 || cargoCapacity < 0 || towingCapacity < 0) {
-            throw InvalidValueException("Invalid value in SUV");
-        }
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const SUV& suv) {
-        suv.display();
-        return os;
-    }
-
-    friend std::istream& operator>>(std::istream& is, SUV& suv) {
-        suv.read(is);
-        std::cout << "Enter Ground Clearance (inches): ";
-        is >> suv.groundClearance;
-        std::cout << "Enter Cargo Capacity (cu ft): ";
-        is >> suv.cargoCapacity;
-        std::cout << "Four-Wheel Drive (1 for Yes, 0 for No): ";
-        is >> suv.fourWheelDrive;
-        std::cout << "Enter Towing Capacity (lbs): ";
-        is >> suv.towingCapacity;
-        return is;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const SUV& suv);
+    friend std::istream& operator>>(std::istream& is, SUV& suv);
 };
 
 #endif // SUV_H
